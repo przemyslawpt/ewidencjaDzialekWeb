ad36ca9ea735e94bca8e4fabd414b59f497a274e
pptaszek <p.ptaszek@dodax.com>
2018-02-09 21:24:23 +0100

    Dodalem strony do zapisu i wyswietlania dzialek

--- src/main/java/pl/ptaszek/controller/AdresWebController.java
+++ src/main/java/pl/ptaszek/controller/AdresWebController.java
@@ -1,14 +1,14 @@
 package pl.ptaszek.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import pl.ptaszek.service.AdresService;
 
-@RestController
+@Controller
 public class AdresWebController {
 
 	@Autowired
---
+++ src/main/webapp/WEB-INF/jsp/zapiszDzialka.jsp
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Zapisz dzialke</title>
+</head>
+<body>
+	<h2>Detale dzialki</h2>
+	<form method="post" action="saveDzialka">
+		Numer : <input type="text" name="numerDzialka" /> 
+		Obreb : <input type="text"name="obrebDzialka">
+		Charakter wladania : <input type="text" name="charakterWladaniaDzialka"> 
+		Udzial : <input type="text" name="udzialDzialka"> 
+		Powierzchnia : <input type="text" name="powierzchniaDzialka"> 
+		<input type="submit" value="Submit">
+	</form>
+</body>
+</html>
