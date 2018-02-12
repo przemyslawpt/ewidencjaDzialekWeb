eb7deb624937c0de921e6a62ffb5b94f1494bf4e
pptaszek <p.ptaszek@dodax.com>
2018-02-12 21:58:58 +0100

    zmiany test

--- src/main/java/pl/ptaszek/config/Application.java
+++ src/main/java/pl/ptaszek/config/Application.java
@@ -26,6 +26,7 @@ public class Application extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		// comment
 		return application.sources(applicationClass);
 	}
 
