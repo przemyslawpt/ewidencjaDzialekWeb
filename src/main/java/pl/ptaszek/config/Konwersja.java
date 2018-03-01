c083193a9d718b8a0d660e6c4d7b132d0282deff
Joanna Ptaszek <Joanna Ptaszek@LAPTOP-AJTT87ME>
2018-03-01 21:37:10 +0100

    komituje

--- src/main/java/pl/ptaszek/config/Konwersja.java
+++ src/main/java/pl/ptaszek/config/Konwersja.java
@@ -7,6 +7,7 @@ import pl.ptaszek.model.Dzialka;
 
 import java.io.*;
 import java.io.File;
+import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +18,7 @@ import java.util.List;
  */
 public class Konwersja {
 
-	private static final String PATH = "C:\\ewidencjaDzialekWeb\\ewidencjaDzialekWeb\\src\\main\\java\\pl\\ptaszek\\config\\";
+	private static final String PATH = "C:\\dzialki\\ewidencjaDzialekWeb\\src\\main\\java\\pl\\ptaszek\\config\\";
 	
 	public static List<Dzialka> stworzDzialkiZPliku() {
 		List<Dzialka> dzialkaList = new ArrayList<>();
@@ -40,29 +41,47 @@ public class Konwersja {
 		return dzialkaList;
 	}
 	
+	public static void wydrukujListaDzialek() {
+		List<Dzialka> dzialki = new ArrayList<>();
+		Dzialka dzialka1 = new Dzialka();
+		dzialka1.setNumerEwidencyjny("12");
+		dzialka1.setObreb("Domaradz");
+		Dzialka dzialka2 = new Dzialka();
+		dzialka2.setNumerEwidencyjny("18");
+		dzialka2.setObreb("Zieleniec");
+		dzialki.add(dzialka1);
+		dzialki.add(dzialka2);
+		String text = "Stan dzialek na dzien 1 grudnia 2016";
+		// zadania stworzyc plik dzialki.csv ktory bedzie zawieraj
+		// linie z text oraz dwie dzialki z wykorzystanie biblioteki BeanIO
+		//plik do mapowania w obecnym katalogu: asiaMapowanie.xml
+		// plik wynikowy z plikiem "csv" otwieralnym w excelu asiaWyniki.csv
+		System.out.println("Gdzie jest plik ? Bierz sie do roboty leniu.");
+	}
+	
 	public static void main(String[] argc) {
-		List<Dzialka> dzialkaList = stworzDzialkiZPliku(); 
+		wydrukujListaDzialek(); 
         Integer licznik = 0;
-        for(Dzialka dzialka: dzialkaList) {
-           System.out.println("----");
-           System.out.println("Nr " + licznik++);
-           System.out.println("Obreb: " + dzialka.getObreb());
-           System.out.println("Numer ewidencyjny: " + dzialka.getNumerEwidencyjny());;
-           System.out.println("rodzajDokumentyPotwierdzajacyWlasnosc: " + dzialka.getWlasnoscRodzajDokumentu());
-           System.out.println("numerDokumentuPotwierdzajacyWlasnosc: " + dzialka.getWlasnoscNumerDokumentu());
-           System.out.println("charakterWladania: " + dzialka.getCharakterWladania());
-           System.out.println("udzialy: " + dzialka.getUdzialy());
-           System.out.println("powierzchniaDzialki: " + dzialka.getPowierzchniaDzialki());
-           System.out.println("powierzchniaZabudowy: " + dzialka.getPowierzchniaZabudowy());
-           System.out.println("przeznaczenie: " + dzialka.getPrzeznaczenie());
-           System.out.println("aktualneWykorzystanie: " + dzialka.getAktualneWykorzystanie());
-           System.out.println("oszacowanaWartoscNieruchomosci: " + dzialka.getOszacowanaWartosc());
-           System.out.println("planWykorzystaniaLata: " + dzialka.getPlanWykorzystaniaLata());
-           System.out.println("uwagi: " + dzialka.getUwagi());
-           System.out.println("skladKomisji: " + dzialka.getSkladKomisji());
-           System.out.println("");
-           System.out.println("----");
-        }
+//        for(Dzialka dzialka: dzialkaList) {
+//           System.out.println("----");
+//           System.out.println("Nr " + licznik++);
+//           System.out.println("Obreb: " + dzialka.getObreb());
+//           System.out.println("Numer ewidencyjny: " + dzialka.getNumerEwidencyjny());;
+//           System.out.println("rodzajDokumentyPotwierdzajacyWlasnosc: " + dzialka.getWlasnoscRodzajDokumentu());
+//           System.out.println("numerDokumentuPotwierdzajacyWlasnosc: " + dzialka.getWlasnoscNumerDokumentu());
+//           System.out.println("charakterWladania: " + dzialka.getCharakterWladania());
+//           System.out.println("udzialy: " + dzialka.getUdzialy());
+//           System.out.println("powierzchniaDzialki: " + dzialka.getPowierzchniaDzialki());
+//           System.out.println("powierzchniaZabudowy: " + dzialka.getPowierzchniaZabudowy());
+//           System.out.println("przeznaczenie: " + dzialka.getPrzeznaczenie());
+//           System.out.println("aktualneWykorzystanie: " + dzialka.getAktualneWykorzystanie());
+//           System.out.println("oszacowanaWartoscNieruchomosci: " + dzialka.getOszacowanaWartosc());
+//           System.out.println("planWykorzystaniaLata: " + dzialka.getPlanWykorzystaniaLata());
+//           System.out.println("uwagi: " + dzialka.getUwagi());
+//           System.out.println("skladKomisji: " + dzialka.getSkladKomisji());
+//           System.out.println("");
+//           System.out.println("----");
+//        }
 /*   		private String numerEwidencyjny; //  C
 		
    		private String obreb; // B
